3e5f97b23fec6f5cd5cfa8be429952949cd48f42
Vladislav <drimacus182@gmail.com>
2018-04-27 18:27:49 +0300

    Фулскрін з зумом для графіків

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
     
     var chart = twitterchart();
     d3.select("#chart-interface-container").call(chart);
-
-
-
-
 })();
 
+$(function() {
+    var viewer = ImageViewer();
+
+    $(".clickable img").click(function() {
+        viewer.show(this.src);
+    });
+});
 
